1e02edaf78fc1c5f0e304698e338e0ea49193cd0
PragatiVerma18 <itispragativerma@gmail.com>
2020-08-01 16:15:51 +0530

    added models

--- AWSAR/Accounts/models.py
+++ AWSAR/Accounts/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from accounts.managers import UserManager
+
+
+ROLE_CHOICES = (
+    ('employer', 'Employer'),
+    ('employee', 'Employee'))
+
+
+class User(AbstractUser):
+    username = models.CharField(max_length=100, unique=True, blank=False, default='', error_messages={
+        'required': "Username must be provided.",
+        'unique': "A user with that username already exists."
+    })
+    role = models.CharField(max_length=12, error_messages={
+        'required': "Role must be provided"
+    })
+    verified = models.BooleanField(default=False)
+    email = models.EmailField(unique=True, blank=False,
+                              error_messages={
+                                  'unique': "A user with that email already exists.",
+                              })
+    is_active = models.BooleanField(default=True)
+
+    REQUIRED_FIELDS = ["email"]
+
+    def __unicode__(self):
+        return self.email
+
+    objects = UserManager()
