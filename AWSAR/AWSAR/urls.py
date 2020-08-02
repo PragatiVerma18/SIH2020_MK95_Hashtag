db1aa65b1b10e131c82e539d6a11b3befc00eb95
Ryuk07 <rohit.edcjss@gmail.com>
2020-08-02 02:52:08 -0700

    awsar urls

--- AWSAR/AWSAR/urls.py
+++ AWSAR/AWSAR/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),
+    path('api/jobs/', include('jobs.api.urls')),
+    path('api/profile/', include('profiles.urls')),
 ]
