6d6cace551bf760c4a11da30a911f1e19f91bf8f
Ryuk07 <rohit.edcjss@gmail.com>
2020-08-02 02:39:05 -0700

    jobs admin , apps and models

--- AWSAR/Jobs/models.py
+++ AWSAR/Jobs/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 from django.utils import timezone
+
 from accounts.models import User
-from profiles.models import EmployeeProfile
+
+JOB_TYPE = (
+    ('1', "Full time"),
+    ('2', "Part time"),
+    ('3', "Internship"),
+)
 
 
 class Job(models.Model):
@@ -10,38 +16,27 @@ class Job(models.Model):
     title = models.CharField(max_length=300)
     description = models.TextField(blank=True)
     location = models.CharField(max_length=150)
-    type = models.CharField(max_length=10)
+    type = models.CharField(choices=JOB_TYPE, max_length=10)
     category = models.CharField(max_length=100)
     last_date = models.DateTimeField()
     company_name = models.CharField(max_length=100)
     vacancies = models.IntegerField(default=1)
     doc_url = models.URLField(blank=True)
-    summary = models.TextField(blank=True)
-    qualification = models.TextField(blank=True)
-    experience = models.PositiveIntegerField(default=0)
-    age_limit = models.CharField(max_length=500, default='age >18')
+    summary = models.CharField(max_length=500, blank=True)
     website = models.CharField(max_length=100, blank=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
     filled = models.BooleanField(default=False)
     salary = models.IntegerField(default=0, blank=True)
-    tags = models.TextField(default='')
-    job_for_women = models.BooleanField(default=False, null=True)
-    job_for_disabled = models.BooleanField(default=False, null=True)
 
     def __str__(self):
         return self.title
 
 
 class Applicant(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, to_field='username')
-    employee = models.OneToOneField(
-        EmployeeProfile, on_delete=models.CASCADE, to_field='user')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     job = models.ForeignKey(Job, on_delete=models.CASCADE,
                             related_name='applicants')
     applied_at = models.DateTimeField(default=timezone.now)
-    status = models.CharField(max_length=15, default='Applied')
 
     def __str__(self):
         return self.user.get_full_name()
