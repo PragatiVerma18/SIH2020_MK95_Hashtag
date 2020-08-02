ce30d035c944c9d69f727053dff149a1ecc49545
Ryuk07 <rohit.edcjss@gmail.com>
2020-08-02 05:50:55 -0700

    profiles urls and views updated

--- AWSAR/Profiles/views.py
+++ AWSAR/Profiles/views.py
@@ -1,3 +1,40 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import EmployeeProfileSerializer, EmployerProfileSerializer
+from .models import EmployeeProfile, WorkExperience, Education, EmployerProfile
 
-# Create your views here.
+
+class EmployeeProfileCreateView(generics.CreateAPIView):
+    serializer_class = EmployeeProfileSerializer
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {'request': self.request}
+
+    def get_queryset(self):
+        user = self.request.user
+        return EmployeeProfile.objects.all()
+
+
+class EmployeeProfileListView(generics.RetrieveUpdateAPIView):
+    serializer_class = EmployeeProfileSerializer
+
+    def get_serializer_context(self, *args, **kwargs):
+        return {'request': self.request}
+
+    def get_queryset(self):
+        user = self.request.user
+        return EmployeeProfile.objects.all()
+
+
+class EmployerProfileCreateView(generics.CreateAPIView):
+    model = EmployerProfile
+    serializer_class = EmployerProfileSerializer
+
+
+class EmployerProfileListView(generics.RetrieveUpdateAPIView):
+    model = EmployerProfile
+    serializer_class = EmployerProfileSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return EmployerProfile.objects.all()
