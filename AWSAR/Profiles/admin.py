6eff3e527acaa305b1a05e583fcd4a5af28f9bab
Ryuk07 <rohit.edcjss@gmail.com>
2020-08-02 03:47:52 -0700

    profiles models

--- AWSAR/Profiles/admin.py
+++ AWSAR/Profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import EmployeeProfile, WorkExperience, Education, EmployerProfile
+
+admin.site.register(EmployeeProfile)
+admin.site.register(WorkExperience)
+
+admin.site.register(Education)
+admin.site.register(EmployerProfile)
--- AWSAR/Profiles/models.py
+++ AWSAR/Profiles/models.py
@@ -1,13 +1,7 @@
-import re
 from django.db import models
 from accounts.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from phonenumber_field.modelfields import PhoneNumberField
-from django.core.validators import RegexValidator
-
-validate_pan = RegexValidator(
-    r'[A-Z]{5}[0-9]{4}[A-Z]{1}', 'Enter a Valid PAN Number')
 
 
 class TimestampedModel(models.Model):
@@ -20,24 +14,14 @@ class TimestampedModel(models.Model):
 
 class EmployeeProfile(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, to_field='username', unique=True)
+        User, on_delete=models.CASCADE, to_field='username')
     first_name = models.CharField(max_length=80)
     last_name = models.CharField(max_length=80)
     gender = models.CharField(max_length=50)
-    about = models.TextField(max_length=500)
-    phone_number = PhoneNumberField(blank=True)
-    email = models.EmailField()
-    dob = models.DateField(null=True)
     title = models.CharField(max_length=100)
     industry = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    skills = models.TextField()
-    portfolio = models.URLField(blank=True)
-    github = models.URLField(blank=True)
-    linkedin = models.URLField(blank=True)
-    twitter = models.URLField(blank=True)
-    image = models.URLField(
-        default='https://www.pngkey.com/png/full/305-3050875_employee-parking-add-employee-icon-png.png')
+    skills = models.CharField(max_length=500)
 
     def __str__(self):
         return self.user.username
@@ -57,41 +41,3 @@ class WorkExperience(models.Model):
 
     class Meta:
         ordering = ['start_date']
-
-
-class Education(models.Model):
-    employee = models.ForeignKey(
-        EmployeeProfile, on_delete=models.CASCADE, related_name='education')
-    institution = models.CharField(max_length=100)
-    degree = models.CharField(max_length=200)
-    start_date = models.DateField()
-    end_date = models.DateField()
-
-    class Meta:
-        ordering = ['start_date']
-
-    def __str__(self):
-        return '%s: %s' % (self.employee, self.degree)
-
-
-class EmployerProfile(models.Model):
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, to_field='username')
-    email = models.EmailField()
-    company_name = models.CharField(max_length=200)
-    full_form = models.CharField(max_length=500, blank=True)
-    location = models.CharField(max_length=100)
-    website = models.URLField(null=True, blank=True)
-    industry = models.CharField(max_length=50)
-    company_size = models.IntegerField()
-    company_type = models.CharField(max_length=50)
-    pan = models.CharField(max_length=10, validators=[
-                           validate_pan], blank=True)
-    overview = models.TextField(max_length=500)
-    linkedin = models.URLField(blank=True)
-    twitter = models.URLField(blank=True)
-    image = models.URLField(
-        default='https://cdn0.iconfinder.com/data/icons/building-vol-9/512/12-512.png')
-
-    def __str__(self):
-        return '%s' % (self.company_name)
