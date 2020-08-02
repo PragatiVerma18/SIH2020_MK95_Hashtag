cd8f298ee53f45412d778aa8f7e83ecfe84daa56
shristisingh29 <44435610+shristisingh29@users.noreply.github.com>
2020-08-02 18:58:58 +0530

    add usePaginate customHook to utils

---
+++ src/utils/customHooks/usePaginate.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export const usePagination = (data, itemsPerPage) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const maxPage = Math.ceil(data.length / itemsPerPage);
+
+  function next() {
+    setCurrentPage((currentPage) => Math.min(currentPage + 1, maxPage));
+  }
+
+  function prev() {
+    setCurrentPage((currentPage) => Math.max(currentPage - 1, 1));
+  }
+
+  function currentData() {
+    const begin = (currentPage - 1) * itemsPerPage;
+    const end = begin + itemsPerPage;
+    return data.slice(begin, end);
+  }
+
+  return { next, prev, currentData, currentPage, maxPage };
+};
